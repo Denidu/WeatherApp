9e2e603b98fab009d7e8a9ff7ea1b4626cde7993
Denidu Gamage <denidugamage21@gmail.com>
2024-09-20 12:42:00 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Denidu Gamage on 2024-09-20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
